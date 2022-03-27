b91af4e301218e5b45ae8b3e8e31df6b1920a99f
jbt <jbt@310-INS>
2022-03-27 14:13:22 +0300

    rectangle

--- 22-03-27-java-eldar/src/d/shapes/Rectangle.java
+++ 22-03-27-java-eldar/src/d/shapes/Rectangle.java
@@ -2,5 +2,17 @@ package d.shapes;
 
 public class Rectangle extends Shape {
 	
+	private double length;
+	private double width;
+	
+	@Override
+	public double getArea() {
+		return length * width;
+	}
+	
+
+	
+	
+	
 	
 }
