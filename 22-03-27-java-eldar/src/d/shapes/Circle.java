fd5b2e7dbb99b7900dcda4770267cd7a96c3fa98
jbt <jbt@310-INS>
2022-03-27 15:28:36 +0300

    aaa

--- 22-03-27-java-eldar/src/d/shapes/Circle.java
+++ 22-03-27-java-eldar/src/d/shapes/Circle.java
@@ -29,7 +29,7 @@ public class Circle extends Shape {
 	@Override
 	public String toString() {
 		return "Circle [radius=" + radius + ", color=" + getColor() + "]";
-	}
+	} 
 	
 	
 
--- 22-03-27-java-eldar/src/d/shapes/Test.java
+++ 22-03-27-java-eldar/src/d/shapes/Test.java
@@ -4,8 +4,24 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Rectangle r = new Rectangle("red", 4, 8);
-		System.out.println(r);
+		// OOP Principles
+		
+		// 1. encapsulation 
+		// 2. inheritance
+		// 3. polymorphism 
+		
+		// declare a reference of type Shape
+		Shape shape;
+
+		// point to an object of type Rectangle
+		shape = new Rectangle("red", 4, 8);
+		System.out.println(shape);
+		System.out.println("Area: " + shape.getArea());
+
+		// point to an object of type Circle
+		shape = new Circle("yellow", 2.3);
+		System.out.println(shape);
+		System.out.println("Area: " + shape.getArea());
 
 	} 
 
