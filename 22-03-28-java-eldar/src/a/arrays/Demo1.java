113da95b9dcb4b51e9e62b1c5063b29536fba901
jbt <jbt@310-INS>
2022-03-28 09:44:04 +0300

    array

---
+++ 22-03-28-java-eldar/src/a/arrays/Demo1.java
@@ -0,0 +1,23 @@
+package a.arrays;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+
+		// create an array of 5 int elements
+		int[] arr1 = new int[5];
+		// populate array with data
+		arr1[0] = 7;
+		arr1[1] = 3;
+		arr1[2] = 9;
+		arr1[3] = 2;
+		arr1[4] = 12;
+		// access array elements (for printing)
+		System.out.println(arr1[0]);
+		System.out.println(arr1[1]);
+		System.out.println(arr1[2]);
+		System.out.println(arr1[3]);
+		System.out.println(arr1[4]);
+
+	}
+}
