0339dc8e0fb26e7a459dbf5c2b280287d654e5f6
jbt <jbt@310-INS>
2022-03-28 12:40:37 +0300

    Arrays

--- 22-03-28-java-eldar/src/a/arrays/Demo8.java
+++ 22-03-28-java-eldar/src/a/arrays/Demo8.java
@@ -1,5 +1,7 @@
 package a.arrays;
 
+import java.util.Arrays;
+
 public class Demo8 {
 
 	// 1. create an array of 10 numbers 0 - 10
@@ -7,8 +9,20 @@ public class Demo8 {
 	// 3. reverse the elements order in the array
 	// 4. print the array again
 	public static void main(String[] args) {
-		
-		
+
+		int[] arr = new int[10];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 11);
+		}
+		System.out.println(Arrays.toString(arr));
+
+		for (int i = 0, j = arr.length - 1; i < arr.length / 2; i++, j--) {
+			int a = arr[i];
+			int b = arr[j];
+			arr[j] = a;
+			arr[i] = b;
+		}
+		System.out.println(Arrays.toString(arr));
 
 	}
 
