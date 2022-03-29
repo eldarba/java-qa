c8498961613629e769aba2da1d2d2b631132d8ed
jbt <jbt@310-INS>
2022-03-29 12:59:17 +0300

    AAA

--- java-knowledge-review/src/e/flow/control/Demo9Tar.java
+++ java-knowledge-review/src/e/flow/control/Demo9Tar.java
@@ -15,9 +15,23 @@ public class Demo9Tar {
 		System.out.println("===Tar 2");
 		// tar 2
 		{
-			
+			for (int i = 100; i <= 125; i += 2) {
+				System.out.print(i + ", ");
+			}
+			System.out.println();
 		}
 		
+		System.out.println("===Tar 3");
+		// tar 3
+		for (int i = 0; i < 5; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.print(j + ", ");
+			}
+			System.out.println(); // new line
+		}
+		 
+		System.out.println("===Tar 4 multiplication table");
+		// tar 4
 
 	}
 
